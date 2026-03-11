8ba3e0e9dc342b3ce6ca48d5cc536c89f79020c1
luisa <luisalvarado250205@gmail.com>
2026-03-11 16:54:17 -0600

    setup

---
+++ src/test/java/org/example/bolsadeempleo/BolsaDeEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bolsadeempleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaDeEmpleoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
